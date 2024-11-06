a0b59d7b710e28358752fe1fd02365b2fbbd1ffe
Adam Wolfe Gordon <awg@upbound.io>
2024-11-06 15:33:40 -0700

    Snake case Python variables

--- functions/compose-container-python/main.py
+++ functions/compose-container-python/main.py
@@ -6,25 +6,25 @@ from .model.io.upbound.azure.storage.container import v1beta1 as contv1beta1
 from .model.com.example.platform.xstoragecontainer import v1alpha1
 
 def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
-    observedXR = v1alpha1.XStorageContainer(**req.observed.composite.resource)
-    xrName = observedXR.metadata.name
-    acctName = xrName + "-account"
+    observed_xr = v1alpha1.XStorageContainer(**req.observed.composite.resource)
+    xr_name = observed_xr.metadata.name
+    acct_name = xr_name + "-account"
 
     acct = acctv1beta1.Account(
         apiVersion="storage.azure.upbound.io/v1beta1",
         kind="Account",
         metadata=metav1.ObjectMeta(
-            name=acctName,
+            name=acct_name,
         ),
         spec=acctv1beta1.Spec(
             forProvider=acctv1beta1.ForProvider(
                 accountTier="Standard",
                 accountReplicationType="LRS",
-                location=observedXR.spec.location,
+                location=observed_xr.spec.location,
                 infrastructureEncryptionEnabled=True,
                 blobProperties=[
                     acctv1beta1.BlobProperty(
-                        versioningEnabled=observedXR.spec.versioning,
+                        versioningEnabled=observed_xr.spec.versioning,
                     ),
                 ],
             ),
@@ -32,12 +32,12 @@ def compose(req: fnv1.RunFunctionRequest, rsp: fnv1.RunFunctionResponse):
     )
     resource.update(rsp.desired.resources[acct.metadata.name], acct)
 
-    accessType = "blob" if observedXR.spec.acl == "public" else "private"
+    accessType = "blob" if observed_xr.spec.acl == "public" else "private"
     cont = contv1beta1.Container(
         apiVersion="storage.azure.upbound.io/v1beta1",
         kind="Container",
         metadata=metav1.ObjectMeta(
-            name=xrName + "-container",
+            name=xr_name + "-container",
         ),
         spec=contv1beta1.Spec(
             forProvider=contv1beta1.ForProvider(
